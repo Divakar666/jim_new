35cb647718912cb83a02fba3328137bbb5f9a599
dhivakarmm@alphasoftz.in <baskar@alphasoftz.in>
2021-08-09 18:31:18 +0530

    initial commit

---
+++ storage/framework/views/06ffbd7b9240d7f1442a956b2935614bbcc089dd.php
@@ -0,0 +1,477 @@
+<style>
+	#small_navs {
+		color: beige;
+		background-color: yellowgreen;
+	}
+
+	.mlr {
+		margin-right: 267px;
+	}
+
+	.top-bar {
+		position: absolute;
+		left: 350px;
+		width: calc(100% - 100px);
+	}
+
+
+	.topnav {
+		overflow: hidden;
+		background-color: #781328;
+		z-index: 9999;
+		margin-left: 32%;
+		padding-right: 5vw;
+	}
+
+	.topnav a {
+		float: left;
+		color: #f2f2f2;
+		text-align: center;
+		/* padding: 14px 16px;
+  text-decoration: none; */
+		font-size: 14px;
+	}
+
+
+
+	.topnav a.active {
+		background-color: #04AA6D;
+		color: white;
+	}
+
+	a.space {
+		padding-left: 15px;
+	}
+
+	a.space1 {
+		padding-right: 8px;
+	}
+
+	.right {
+		float: right;
+	}
+</style>
+<!--Header-->
+<!--SMALL NAVS-->
+
+<div class="topnav">
+	<div class="container">
+		<div class="social-1">
+			<a href="index.html" class="space">
+				<i class="fa fa-envelope-open "></i> info@jim.ac.in
+			</a>
+			<span class="mlr"></span>
+			<a href="index.html" class="space">
+				<i class="fa fa-phone"></i> +91 431 422 6481
+			</a>
+
+			<a href="index.html" class="space">
+				<i class="fa fa-sign-in"></i> Login
+			</a>
+			<a href="index.html" class="space">
+				<i class="fa fa-graduation-cap"></i> Admission
+			</a>
+
+		</div>
+
+	</div>
+</div>
+
+<!--SMALL NAVS-->
+<div class="transform">
+	<div id="header-sroll">
+		<nav class="navbar navbar-expand-md header navbar-offcanvas mega-menu only_for_home">
+
+			<div class="container-fluid container-custom"> <a id="0000" style="display:none;" class="navbar-brand" href="index.html">
+					<img src="assets/jm/images/JM_logo.png" alt="JM logo" title="JM logo">
+
+				</a>
+
+
+				<ul class="navbar-nav navbar-top" id="dfd" style="display:none;">
+
+					<!--about section li start from here-->
+					<!-- Home Menu Start-->
+					<li class="nav-item mega_hover_effect">
+						<a class="nav-link" href="javascript:;" id="hme">Home</a>
+						<!-- Home  Menu End  -->
+						<!-- Academic  Menu Start  -->
+					<li class="nav-item mega_hover_effect">
+						<a class="nav-link" href="javascript:;" id="prg">Academics </a>
+						<div class="on_hover mega-menu-main">
+							<div class="container-custom mega-container">
+								<div class="left_menu">
+									<div class="mega_menu_images_holder"><img alt="Asia’s 1 st LEED Platinum Rated Campus" src="assets/jm/images/about_mega.jpg" title="Asia’s 1 st LEED Platinum Rated Campus" /></div>
+
+									<p class="img-text" style=" font-family: montserratextrabold; text-transform: uppercase;">Asia’s 1<sup style="font-size:20px;">st</sup> LEED Platinum Rated Campus</p>
+								</div>
+
+								<div class="right_menu">
+									<div class="mega_menu_part_inner_holder">
+										<div class="row">
+											<div class="col-sm-6">
+												<h3 class="mega-menu-head2 mega-menu-head-pad"><a href="about-us.html">MBA</a></h3>
+
+												<ul class="mega_menu_margin_top10">
+													<li><a href="about-us/chairmans-message.html">Syllabus</a></li>
+
+													<li><a href="about-us/vice-chairman-and-ceo-message.html">Course Allotment</a></li>
+
+												</ul>
+
+												<h3 class="mega-menu-head2 term-zero-menu"><a href="term-zero.html">Ph.D</a></h3>
+
+												<div class="clear"> </div>
+
+												<h3 class="mega_menu_margin_top30 mega-menu-head-pad"><a href="about-media.html">Research</a></h3>
+
+												<ul class="mega_menu_margin_top10">
+													<!--<li><a href="https://www.greatlakes.edu.in/chennai/news">News </a></li>-->
+													<li><a href="events.html">Overview</a></li>
+													<li><a href="in-media.html">Research Advisers</a></li>
+													<li><a href="about-us/media-kit.html">Minor / Major projects</a></li>
+												</ul>
+
+				
+
+											</div>
+
+											<div class="col-sm-6">
+												<div class="mega_menu_part_inner_holder border-none  padding-right-mega">
+													<h3 class="mega-menu-head2"><a href="rankings.html">Faculty</a></h3>
+
+													<div class="clear"> </div>
+
+													<h3 class="mega-menu-head2"><a href="accreditations.html">Guest Speakers</a></h3>
+
+													<div class="clear"> </div>
+													
+
+					
+												</div>
+
+												<h3 class="mega_menu_margin_top30 mega-menu-head-pad"><a href="about-media.html">Learning</a></h3>
+
+<ul class="mega_menu_margin_top10">
+	<!--<li><a href="https://www.greatlakes.edu.in/chennai/news">News </a></li>-->
+	<li><a href="events.html">Communication Course</a></li>
+	<li><a href="in-media.html">Bridge Course</a></li>
+	<li><a href="about-us/media-kit.html">Personal Growth Lab</a></li>
+	<li><a href="events.html">Skill Enhancement Programme</a></li>
+	<li><a href="in-media.html">The Industry Institute Interface</a></li>
+	<li><a href="about-us/media-kit.html">Assessment of Learning</a></li>
+	<li><a href="about-us/media-kit.html">Learning Academy</a></li>
+	<li><a href="about-us/media-kit.html">Industry Ready Training</a></li>
+	<li><a href="about-us/media-kit.html">Social Involvement Programme</a></li>
+</ul>
+											</div>
+										</div>
+
+										<div class="inner_main_mega_menu"> </div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<!-- Academic  Menu End  -->
+					<!-- Faculty  Menu Start  -->
+					<li class="nav-item mega_hover_effect">
+						<a class="nav-link" href="javascript:;" id="fac">FACULITIES</a>
+						<div class="on_hover mega-menu-main">
+							<div class="container-custom mega-container">
+								<div class="left_menu">
+									<div class="mega_menu_images_holder"><img alt="Asia’s 1 st LEED Platinum Rated Campus" src="assets/jm/images/about_mega.jpg" title="Asia’s 1 st LEED Platinum Rated Campus" /></div>
+
+									<p class="img-text" style=" font-family: montserratextrabold; text-transform: uppercase;">Asia’s 1<sup style="font-size:20px;">st</sup> LEED Platinum Rated Campus</p>
+								</div>
+
+								<div class="right_menu">
+									<div class="mega_menu_part_inner_holder">
+										<div class="row">
+											<div class="col-sm-6">
+											<h3 class="mega-menu-head2"><a href="accreditations.html">Board Room</a></h3>
+
+<div class="clear"> </div>
+<h3 class="mega-menu-head2"><a href="accreditations.html">Hostels</a></h3>
+
+<div class="clear"> </div>
+<h3 class="mega-menu-head2"><a href="accreditations.html">WIFI Campus</a></h3>
+
+<div class="clear"> </div>
+
+<h3 class="mega-menu-head2"><a href="accreditations.html">Student Affinites</a></h3>
+
+<div class="clear"> </div>
+
+<h3 class="mega-menu-head2"><a href="accreditations.html">Research Room</a></h3>
+
+<div class="clear"> </div>
+											</div>
+												
+											<div class="col-sm-6">
+												<div class="mega_menu_part_inner_holder border-none  padding-right-mega">													<h3 class="mega-menu-head2"><a href="rankings.html">The Campus</a></h3>
+
+													<div class="clear"> </div>
+
+													<h3 class="mega-menu-head2"><a href="accreditations.html">Computer Lab</a></h3>
+
+													<div class="clear"> </div>
+													<h3 class="mega-menu-head2"><a href="accreditations.html">The Library</a></h3>
+
+<div class="clear"> </div>	
+<h3 class="mega-menu-head2"><a href="accreditations.html">Auditorium</a></h3>
+
+
+
+					
+												</div>
+
+												
+											</div>
+										</div>
+
+										<div class="inner_main_mega_menu"> </div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<!-- Faculty  Menu End  -->
+					<!-- Traning And placements  Menu Start  -->
+					<li class="nav-item mega_hover_effect">
+						<a rel="nofollow" class="nav-link disabled" href="#" target="">Students</a>
+
+						<div class="on_hover mega-menu-main mega-menu-main_new">
+							<div class="container-custom mega-container">
+								<div class="left_menu">
+									<div class="mega_menu_images_holder"><img alt="Transform and Lead" src="assets/jm/images/program-bg.jpg" title="Transform and Lead" /></div>
+
+									<p class="img-text" style=" font-family: montserratextrabold;    text-transform: uppercase;">Transform and Lead</p>
+								</div>
+
+								<div class="right_menu">
+									<div class="row">
+										<div class="col-sm-6 mega-menu-part">
+											<div class="mega_menu_part_inner_holder">
+												<div class="row">
+													<div class="col-sm-12">
+														<h3 class="mega-menu-head2 mega-menu-head2_new"><a href="full-time-programs.html">Campus Ministry</a></h3>
+
+														<div class="mega_menu_margin mega_menu_margin_para mega_menu_margin_top40">
+					
+
+															<h3 class="mega_menu_margin_top30 mega-menu-head-pad"><a href="about-media.html">Co-Curricular Activites</a></h3>
+
+<ul class="mega_menu_margin_top10">
+	<!--<li><a href="https://www.greatlakes.edu.in/chennai/news">News </a></li>-->
+	<li><a href="events.html">Shepherd</a></li>
+	<li><a href="in-media.html">JIMNESIA</a></li>
+	<li><a href="about-us/media-kit.html">Rotaract Club</a></li>
+	<li><a href="events.html">Sports</a></li>
+</ul>
+
+															<div class="clear"> </div>
+
+	
+
+														</div>
+													</div>
+												</div>
+
+												<div class="inner_main_mega_menu"> </div>
+											</div>
+										</div>
+
+										<div class="col-sm-6 mega-menu-part">
+											<div class="mega_menu_part_inner_holder padding-right-mega">
+												<h3 class="mega-menu-head2 mega-menu-head2_new"><a href="corporate-programs.html">SAC</a></h3>
+
+												<div class="mega_menu_margin mega_menu_margin_para mega_menu_margin_top40">
+													<h4 class="mega-menu-head2 mega-menu-head-pad"><a href="pgxpm.html">Club Activities</a></h4>
+		
+
+													<div class="clear"> </div>
+
+													<h3 class="mega_menu_margin_top30 mega-menu-head-pad"><a href="about-media.html">Training</a></h3>
+
+<ul class="mega_menu_margin_top10">
+	<!--<li><a href="https://www.greatlakes.edu.in/chennai/news">News </a></li>-->
+	<li><a href="events.html">MDP</a></li>
+	<li><a href="in-media.html">FDP</a></li>
+	<li><a href="about-us/media-kit.html">ICP</a></li>
+	<li><a href="events.html">MQAC</a></li>
+</ul>
+
+													<div class="clear"> </div>
+
+													<div class="clear"> </div>
+
+													<div class="clear"> </div>
+
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<!-- Traning And placements  Menu End  -->
+					<!-- Life@jim  Menu Start  -->
+					<li class="nav-item mega_hover_effect"> <a rel="nofollow" class="nav-link disabled" href="#" target="">Placements</a>
+
+						<div class="on_hover mega-menu-main mega-menu-main_new">
+							<div class="container-custom mega-container">
+								<div class="left_menu">
+									<div class="mega_menu_images_holder"><img alt="Transform and Lead" src="assets/jm/images/program-bg.jpg" title="Transform and Lead" /></div>
+									<p class="img-text" style=" font-family: montserratextrabold;    text-transform: uppercase;">Transform and Lead</p>
+								</div>
+
+								<div class="right_menu">
+									<div class="row">
+										<div class="col-sm-6 mega-menu-part">
+											<div class="mega_menu_part_inner_holder">
+												<div class="row">
+													<div class="col-sm-12">
+														<h3 class="mega-menu-head2 mega-menu-head2_new"><a href="full-time-programs.html">Our Recruiters</a></h3>
+
+														<div class="mega_menu_margin mega_menu_margin_para mega_menu_margin_top40">
+	
+
+
+
+															<div class="clear"> </div>
+
+															
+															<div class="clear"> </div>
+
+										
+														</div>
+													</div>
+												</div>
+
+												<div class="inner_main_mega_menu"> </div>
+											</div>
+										</div>
+
+										<div class="col-sm-6 mega-menu-part">
+											<div class="mega_menu_part_inner_holder padding-right-mega">
+												<h3 class="mega-menu-head2 mega-menu-head2_new"><a href="corporate-programs.html">Brochure</a></h3>
+
+												<div class="mega_menu_margin mega_menu_margin_para mega_menu_margin_top40">
+										
+													<div class="clear"> </div>
+
+						
+													<div class="clear"> </div>
+
+												
+
+													<div class="clear"> </div>
+
+
+
+													<div class="clear"> </div>
+
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<!-- Life@JIM  Menu End  -->
+					<!-- About   Menu Start  -->
+					<li class="nav-item mega_hover_effect"> <a rel="nofollow" class="nav-link disabled" href="#" target="">Life @ JIM</a>
+
+						<div class="on_hover mega-menu-main mega-menu-main_new">
+							<div class="container-custom mega-container">
+								<div class="left_menu">
+									<div class="mega_menu_images_holder"><img alt="Transform and Lead" src="assets/jm/images/program-bg.jpg" title="Transform and Lead" /></div>
+									<p class="img-text" style=" font-family: montserratextrabold;    text-transform: uppercase;">Transform and Lead</p>
+								</div>
+
+								<div class="right_menu">
+									<div class="row">
+										<div class="col-sm-6 mega-menu-part">
+											<div class="mega_menu_part_inner_holder">
+												<div class="row">
+													<div class="col-sm-12">
+														<div class="mega_menu_margin mega_menu_margin_para mega_menu_margin_top40">
+															<h4 class="mega-menu-head2 mega-menu-head-pad"><a href="pgpm.html">Photo Gallery</a></h4>
+
+														
+															<a class="menu-read-more pgpmmegamenulm" href="pgpm.html">Open Gallery <i class="fa fa-angle-right pgpmmegamenulm" aria-hidden="true"></i></a>
+															<!--<span class="date-div-col">APP. Deadline: 14<sup>th</sup> April, 2021</span>-->
+															<!-- <span class="date-div-col">Admissions Open!</span> -->
+
+
+
+															<div class="clear"> </div>
+
+														
+
+															<div class="clear"> </div>
+
+														
+
+														</div>
+													</div>
+												</div>
+
+												<div class="inner_main_mega_menu"> </div>
+											</div>
+										</div>
+
+										<div class="col-sm-6 mega-menu-part">
+											<div class="mega_menu_part_inner_holder padding-right-mega">
+										
+												<div class="mega_menu_margin mega_menu_margin_para mega_menu_margin_top40">
+									
+													<div class="clear"> </div>
+
+													<div class="clear"> </div>
+
+												
+
+													<div class="clear"> </div>
+
+
+													
+
+													<div class="clear"> </div>
+
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<!--About  Menu End  -->
+					<!--Contact  Menu Start  -->
+					<li class="nav-item mega_hover_effect"> <a rel="nofollow" class="nav-link disabled" href="#" target="">Contact</a>
+			
+					</li>
+					<!-- COntact Menu End  -->
+				</ul>
+				<div class="column">
+					<div id="sb-search" class="sb-search">
+						<form action="https://www.greatlakes.edu.in/chennai/search" method="get">
+							<input class="sb-search-input" placeholder="Search" type="text" value="" name="keywords" id="keywords">
+							<input class="sb-search-submit" type="submit" value="">
+							<!-- <span class="sb-icon-search" id="zzz" style="display:none;"><img src="assets/jm/images/new-search.png" alt="Search Icon" title="Search Icon"/></span> -->
+						</form>
+					</div>
+				</div>
+
+				<bdo id="wwww" style="display:none;">
+					<!-- <a href="#" id="offcanv_menu_1" class="pgpm-menu home_page only-home-page" onClick="disableDiv();"></a> -->
+				</bdo>
+			</div>
+		</nav>
+	</div>
+</div>
+<!--Header--><?php /**PATH /var/www/html/laravel_app/jm/resources/views/includes/header.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/6dfd766312605aa92e75fb9390d3343efd19dad4.php
@@ -0,0 +1,12 @@
+<!-- load bootstrap from a cdn -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo $__env->yieldContent('title'); ?></title>
+ 
+<link rel="shortcut icon" href="<?php echo e(url('assets/jm/icons/favicon.ico')); ?>" type="image/x-icon">
+
+
+<!--Custom css-->
+<link rel="stylesheet" href="<?php echo e(url('assets/jm/css/bootstrap.min.css')); ?>" type="text/css" media="all" >
+<link rel="stylesheet" href="<?php echo e(url('assets/jm/css/style.css')); ?>" type="text/css" media="all"><?php /**PATH /var/www/html/laravel_app/jm/resources/views/includes/head.blade.php ENDPATH**/ ?>
